75182387537440352d8b71de6184e97a7df5641f
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-05-10 12:00:32 +0900

    refileの調整

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '731a082d734cb0650fddf591bb5a191d9ffd656dbed3fd74e52edc09da1fe61775e2c064204cbe78d5cb8e9b7a2be649dd6db195ff116fedc260aacc44c8d346'
