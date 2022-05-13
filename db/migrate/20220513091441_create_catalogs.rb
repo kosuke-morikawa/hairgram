b6256390616cf3fa8e609618a767f1f557082dfe
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-05-13 18:20:47 +0900

    モデル作成

---
+++ db/migrate/20220513091441_create_catalogs.rb
@@ -0,0 +1,12 @@
+class CreateCatalogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :catalogs do |t|
+      t.integer :user_id
+      t.string :title
+      t.text :body
+      t.string :image_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_26_091351) do
+ActiveRecord::Schema.define(version: 2022_05_13_091441) do
+
+  create_table "catalogs", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.text "body"
+    t.string "image_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
