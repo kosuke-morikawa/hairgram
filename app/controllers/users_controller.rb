0c66d4c75b7e97dcf57740aa99d37f533ec245eb
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-06-14 17:47:49 +0900

    updateアクションのフラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user), notices: '投稿に成功しました。'
+      redirect_to user_path(@user), notice: '更新に成功しました。'
     else
       render :edit
     end
