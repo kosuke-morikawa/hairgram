6b5170a07cbd9c60ff8e325183848e416294e58f
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-06-18 23:10:34 +0900

    コントローラ　createアクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,6 @@
 class FavoritesController < ApplicationController
+  def create
+    @favorite = current_user.favorites.create(catalog_id: params[:catalog_id])
+    redirect_back(fallback_location: root_path)
+  end
 end
