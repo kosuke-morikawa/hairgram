97052482c18062055adaa6e6af9b370457aeaf07
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-06-19 22:30:42 +0900

    コントローラ　destroyアクション定義

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,4 +3,11 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.create(catalog_id: params[:catalog_id])
     redirect_back(fallback_location: root_path)
   end
+
+  def destroy
+    @catalog = Catalog.find(params[:catalog_id])
+    @favotite = current_user.favorites.find_by(catalog_id: @catalog.id)
+    @favotite.destroy
+    redirect_back(fallback_location: root_path)
+  end
 end
