244595c17ecb3017654577175d47de86cebff0b5
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-07-09 07:42:53 +0900

    削除

--- app/models/catalog.rb
+++ app/models/catalog.rb
@@ -2,14 +2,6 @@ class Catalog < ApplicationRecord
   belongs_to :user
   attachment :image
   has_many :favorites, dependent: :destroy
-
-  def self.search(search)
-    if search != ""
-      Catalog.where('text LIKE(?)', "%#{search}%")
-    else
-      Catalog.all
-    end
-  end
   
   with_options presence: true do
     validates :title
