e07a3edcc487f9cf933b8e76af2c4d30aa165575
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-07-13 01:19:41 +0900

    userモデル、アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   attachment :profile_image
   has_many :catalogs, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :comments
   
   validates :username, presence: true
 
