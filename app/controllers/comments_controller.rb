2ce43b76ee155821e75c3c148e15de4484221d63
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-07-19 19:37:17 +0900

    修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,6 @@ class CommentsController < ApplicationController
     redirect_to "/catalogs/#{comment.catalog.id}"
   end
   
-
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, catalog_id: params[:catalog_id])
