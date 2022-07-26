112017480baf89af1a2830f5ddbf05128ca38a8c
kosuke-morikawa <haoyousenchuan@gmail.com>
2022-07-27 00:08:41 +0900

    投稿時の遷移先

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     Comment.create(comment_params)
-    redirect_to "/catalogs/#{comment.catalog.id}"
+    redirect_back(fallback_location: root_path)
   end
   
   private
